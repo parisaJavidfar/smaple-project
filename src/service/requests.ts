2f872b8afde7b88ace285ee330ff01bba1b26fd3
p.javidfar <the.mojilla@gmail.com>
2025-01-17 00:04:36 +0330

    chore: create api requests

---
+++ src/service/requests.ts
@@ -0,0 +1,46 @@
+import { useHttp } from "./http";
+import { coinTransformers } from "./coinTransformers.ts";
+import { ENDPOINTS } from "./constants";
+
+import type {
+  CoinsResponse,
+  RawCoinsResponse,
+  CoinHistoryResponse,
+  RawCoinHistoryResponse,
+} from "./schema";
+
+const http = useHttp();
+
+export async function getCoins(): Promise<CoinsResponse> {
+  try {
+    const response = await http.get<RawCoinsResponse>(ENDPOINTS.COINS, {
+      query: {
+        limit: 10,
+      },
+    });
+
+    return response.data.map((coin) => coinTransformers(coin));
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to fetch coins");
+  }
+}
+
+export async function getCoinHistory(
+  coinId: string,
+): Promise<CoinHistoryResponse> {
+  try {
+    const response = await http.get<RawCoinHistoryResponse>(
+      ENDPOINTS.HISTORY.replace(":id", coinId),
+      {
+        query: {
+          interval: "d1",
+        },
+      },
+    );
+
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to get coin history");
+  }
+}
