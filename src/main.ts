892b43444a27a57689c52339c2e9feac9d00dfb4
p.javidfar <the.mojilla@gmail.com>
2025-01-17 00:04:18 +0330

    chore: load plugins dynamically in main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
-import { createApp } from "vue";
 import "./style.css";
-import App from "./App.vue";
-import { router } from "./router";
+import { createApp, type Plugin } from "vue";
 
-createApp(App).use(router).mount("#app");
+import App from "./App.vue";
+
+const app = createApp(App);
+
+Object.values<Plugin>(import.meta.glob("./plugins/*.ts", { eager: true })).map(
+  (plugin) => plugin.install?.(app),
+);
+
+app.mount("#app");
