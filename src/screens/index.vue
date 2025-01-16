9412447da6c02b7b06659ea484257ad19f0dd8b7
p.javidfar <the.mojilla@gmail.com>
2025-01-17 00:08:52 +0330

    feat: create index screen

--- src/screens/index.vue
+++ src/screens/index.vue
@@ -1,8 +1,42 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  hello
+  <div class="p-6" style="height: 100vh">
+    <v-btn @click="toggleTheme">ToggleTheme</v-btn>
+
+    <Table :headers="headers" :data="coinsStore.coins" />
+  </div>
 </template>
 
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useTheme } from "vuetify";
+
+import Table from "../components/Table.vue";
+
+import { useGetCoins } from "../composable/web/useGetCoins.ts";
+
+import { useCoinsStore } from "../stores/coins.ts";
+import { connectWebSocket } from "../service/socket.ts";
+
+const theme = useTheme();
+
+const coinsStore = useCoinsStore();
+const { fetchCoins } = useGetCoins();
+
+const headers = [
+  { title: "Id", key: "id", sortable: true },
+  { title: "Name", key: "name", sortable: true },
+  { title: "Symbol", key: "symbol", sortable: true },
+  { title: "Current Price", key: "currentPrice", sortable: true },
+  { title: "24h Volume", key: "totalVolume", sortable: true },
+];
+
+const toggleTheme = () => {
+  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+};
+
+onMounted(async () => {
+  await fetchCoins();
+
+  connectWebSocket();
+});
+</script>
